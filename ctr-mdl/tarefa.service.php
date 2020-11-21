f837d91dadd66333ec78cd3f141174f58902fed0
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2020-11-20 22:26:51 -0300

    Classe tarefa.servico (CRUD)

---
+++ ctr-mdl/tarefa.service.php
@@ -0,0 +1,20 @@
+<?php
+
+	class TarefaService
+	{
+		public function inserir(){ // Create
+
+		}
+
+		public function recuperar(){ // Read
+
+		}
+
+		public function atualizar(){ //Update
+
+		}
+
+		public function remover(){ //Delete
+
+		}
+	}
