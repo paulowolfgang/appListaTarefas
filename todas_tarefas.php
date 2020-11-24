a01da0eba4a2ee4cce921ec118e5104d0b66916c
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2020-11-24 16:00:05 -0300

    Edição de registros 01

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -17,6 +17,65 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+			function editar(id, txt_tarefa){
+				// alert('Ok');
+				
+				// form de edição
+				let form = document.createElement('form');
+				form.action = '#';
+				form.method = 'post';
+				form.className = 'row';
+
+				// input para a entrada do texto
+				let inputTarefa = document.createElement('input');
+				inputTarefa.type = 'text';
+				inputTarefa.name = 'tarefa';
+				inputTarefa.className = 'col-9 form-control';
+				inputTarefa.value = txt_tarefa;
+
+				// input hiddden para guardar o id da tarefa
+				let inputId = document.createElement('input');
+				inputId.type = 'hidden';
+				inputId.name = 'id';
+				inputId.value = id;
+
+				// button p/ envio do form
+				let button = document.createElement('button');
+				button.type = 'submit';
+				button.className = 'col-3 btn btn-info';
+				button.innerHTML = 'Atualizar';
+
+				// Incluir inputTarefa no form
+				form.appendChild(inputTarefa);
+
+				// Incluir inputId no form
+				form.appendChild(inputId);
+
+				// Incluir o button no form
+				form.appendChild(button);
+
+				/* Tetse
+				console.log(form);
+				alert(id); */
+
+				// Selecionar a div tarefa
+				let tarefa = document.getElementById('tarefa_'+id);
+
+				// Limpa o texto da tarefa para incluir o form
+				tarefa.innerHTML = '';
+
+				// Incluir o form na página
+				tarefa.insertBefore(form, tarefa[0]);
+
+				/* Teste
+				alert(txt_tarefa); */
+
+
+			}
+		</script>
+
 	</head>
 
 	<body>
@@ -49,10 +108,12 @@
 								<?php foreach($tarefas as $key => $tarefa){ ?>
 
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
+										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
+										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
