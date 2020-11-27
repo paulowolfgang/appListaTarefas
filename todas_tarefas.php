7670be966cb0b02337d913aa56a94f8aef705a69
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2020-11-27 20:09:56 -0300

    Tarefas realizadas 01

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -77,6 +77,10 @@
 				location.href = 'todas_tarefas.php?acao=remover&id='+id;
 			}
 
+			function marcarRealizada(id){
+				location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
+			}
+
 		</script>
 
 	</head>
@@ -116,8 +120,12 @@
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
-											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-											<i class="fas fa-check-square fa-lg text-success"></i>
+
+											<?php if($tarefa->status == 'pendente') { ?>
+												<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+												<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+											<?php } ?>
+
 										</div>
 									</div>
 
